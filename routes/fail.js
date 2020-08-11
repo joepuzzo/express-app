4f27a895b8e7d103a7d6582957f66b593544a6f3
Joe Puzzo <jpuzzo@tesla.com>
2020-08-11 09:41:39 -0700

    added catch endpoint to fail

--- routes/fail.js
+++ routes/fail.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import fs from 'fs';
+import logger from 'winston';
 const router = express.Router();
 
 // This will crash app completley
@@ -13,6 +14,16 @@ router.get('/crash', (req, res) => {
 // This will just throw normal error
 router.get('/throw', () => {
     throw new Error('Ahhhh!!!!!');
+});
+
+// This will just throw, catch, and log an error
+router.get('/catch', (req, res) => {
+    try{
+        throw new Error('Ahhhh!!!!!');
+    } catch(e) {
+        logger.error('Recieved and error when trying to do something', e );
+        res.sendStatus(500);
+    }
 });
 
 export default router;
--- routes/health.js
+++ routes/health.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get('/health', (req, res) => {
   const status = { status: 'UP' };
   logger.info('Health', status);
+  logger.debug('Health', status);
   return res.send(status);
 });
 
